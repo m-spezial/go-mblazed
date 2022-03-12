bd62fcbf50606cd14d759cd23793384b170aa0ef
Leo Drachenfeuer <leo.drachenfeuer@m-spezial.de>
2022-03-12 16:15:59 +0100

    refactor: Rename implementation of CoreEngine interface from  coreEngine to Engine

--- core/engine.go
+++ core/engine.go
@@ -7,31 +7,28 @@ import (
 	"net/http"
 )
 
-type RequestHandler func(r logic.RequestContextInterface)
-type RequestContextProcessor func(ctx logic.RequestContextInterface) logic.RequestContextInterface
-
-type coreEngine struct {
+type Engine struct {
 	router                       *Router
-	requestContextProcessorChain []RequestContextProcessor
+	requestContextProcessorChain []logic.RequestContextProcessor
 }
 
-func NewCoreEngine() logic.CoreEngine {
-	return &coreEngine{
+func NewEngine() logic.CoreEngine {
+	return &Engine{
 		router: NewRouter(),
 	}
 }
 
-func (ce coreEngine) GetDb() {
+func (ce *Engine) GetDb() {
 	//TODO implement me
 	panic("implement me")
 }
 
-func (ce coreEngine) GetRenderEngine() logic.RenderEngine {
+func (ce *Engine) GetRenderEngine() logic.RenderEngine {
 	//TODO implement me
 	panic("implement me")
 }
 
-func (ce *coreEngine) wrapRequestHandle(handle RequestHandler) httprouter.Handle {
+func (ce *Engine) wrapRequestHandle(handle logic.RequestHandler) httprouter.Handle {
 	return func(writer http.ResponseWriter, request *http.Request, params httprouter.Params) {
 		var ctx logic.RequestContextInterface
 		ctx = NewRequestContext(writer, request, params)
@@ -43,45 +40,45 @@ func (ce *coreEngine) wrapRequestHandle(handle RequestHandler) httprouter.Handle
 }
 
 // GET is a shortcut for ce.Handle(http.MethodGet, path, handle)
-func (ce *coreEngine) GET(pathname string, path string, handle RequestHandler) {
+func (ce *Engine) GET(pathname string, path string, handle logic.RequestHandler) {
 	ce.Handle(http.MethodGet, pathname, path, handle)
 }
 
 // HEAD is a shortcut for ce.Handle(http.MethodHead, path, handle)
-func (ce *coreEngine) HEAD(pathname string, path string, handle RequestHandler) {
+func (ce *Engine) HEAD(pathname string, path string, handle logic.RequestHandler) {
 	ce.Handle(http.MethodHead, pathname, path, handle)
 }
 
 // OPTIONS is a shortcut for ce.Handle(http.MethodOptions, path, handle)
-func (ce *coreEngine) OPTIONS(pathname string, path string, handle RequestHandler) {
+func (ce *Engine) OPTIONS(pathname string, path string, handle logic.RequestHandler) {
 	ce.Handle(http.MethodOptions, pathname, path, handle)
 }
 
 // POST is a shortcut for ce.Handle(http.MethodPost, path, handle)
-func (ce *coreEngine) POST(pathname string, path string, handle RequestHandler) {
+func (ce *Engine) POST(pathname string, path string, handle logic.RequestHandler) {
 	ce.Handle(http.MethodPost, pathname, path, handle)
 }
 
 // PUT is a shortcut for ce.Handle(http.MethodPut, path, handle)
-func (ce *coreEngine) PUT(pathname string, path string, handle RequestHandler) {
+func (ce *Engine) PUT(pathname string, path string, handle logic.RequestHandler) {
 	ce.Handle(http.MethodPut, pathname, path, handle)
 }
 
 // PATCH is a shortcut for ce.Handle(http.MethodPatch, path, handle)
-func (ce *coreEngine) PATCH(pathname string, path string, handle RequestHandler) {
+func (ce *Engine) PATCH(pathname string, path string, handle logic.RequestHandler) {
 	ce.Handle(http.MethodPatch, pathname, path, handle)
 }
 
 // DELETE is a shortcut for ce.Handle(http.MethodDelete, path, handle)
-func (ce *coreEngine) DELETE(pathname string, path string, handle RequestHandler) {
+func (ce *Engine) DELETE(pathname string, path string, handle logic.RequestHandler) {
 	ce.Handle(http.MethodDelete, pathname, path, handle)
 }
 
-func (ce *coreEngine) Handle(methode string, pathname string, path string, handle RequestHandler) {
+func (ce *Engine) Handle(methode string, pathname string, path string, handle logic.RequestHandler) {
 	ce.router.Handle(methode, pathname, path, ce.wrapRequestHandle(handle))
 }
 
-func (ce *coreEngine) ServeHTTP(writer http.ResponseWriter, request *http.Request) {
+func (ce *Engine) ServeHTTP(writer http.ResponseWriter, request *http.Request) {
 	log.Println("[mblazed] incoming request:" + request.URL.Path)
 	ce.router.ServeHTTP(writer, request)
 }
