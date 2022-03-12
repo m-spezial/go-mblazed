7a00c7ebe07a5d557e7d0f68cb41de8b44676629
Leo Drachenfeuer <leo.drachenfeuer@m-spezial.de>
2022-03-12 16:12:43 +0100

    fix missing defintions in CoreEngine Interface

--- logic/engine.go
+++ logic/engine.go
@@ -1,6 +1,16 @@
 package logic
 
+import "net/http"
+
 type CoreEngine interface {
+	HEAD(pathname string, path string, handle RequestHandler)
+	GET(pathname string, path string, handle RequestHandler)
+	PUT(pathname string, path string, handle RequestHandler)
+	POST(pathname string, path string, handle RequestHandler)
+	PATCH(pathname string, path string, handle RequestHandler)
+	DELETE(pathname string, path string, handle RequestHandler)
+	OPTIONS(pathname string, path string, handle RequestHandler)
+	http.Handler
 	GetDb()
 	GetRenderEngine() RenderEngine
 }
